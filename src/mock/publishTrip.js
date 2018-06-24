ab5a96f6ce79be5957137bd2a398180ad1893689
jindingwen <jindw@outlook.com>
2018-06-24 11:29:24 +0800

    提交首页

---
+++ src/mock/publishTrip.js
@@ -0,0 +1,14 @@
+/**
+ * 发布页数据
+ */
+import Mock from 'mockjs';
+const Random = Mock.Random;
+
+export default () => {
+    return {
+        data: {
+            code: 20000,
+            msg: 'success'
+        }
+    }
+}
