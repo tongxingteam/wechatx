232a201b7d4a2d32d2c2f7abd13b3e45c59db7df
bug10000 <1456822719@qq.com>
2018-07-08 11:42:00 +0800

    本地提交

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    base_url: 'https://wxtx.ilvdudu.com/',
+    login: 'v1/login',//登录接口
+    queryTripListByWord: 'v1/queryTripListByWord',//搜索行程接口
+    queryTripDetail: 'v1/queryTripDetail',//约伴详情接口
+  };
